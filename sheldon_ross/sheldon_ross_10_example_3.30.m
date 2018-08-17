0ff28eeea14fec129857e615aff18169a91c256c
fermibot <ashwinuoh@gmail.com>
2018-08-16 21:01:20 -0500

    Added a new package and a new file

---
+++ sheldon_ross/sheldon_ross_10_example_3.30.m
@@ -0,0 +1,21 @@
+AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
+Needs["statistics`"];
+
+Module[{probabilities =
+   unitize[PDF[PoissonDistribution[25], #] & /@ Range[1, 50]] ->
+    Range[1, 50], samples, plotData, plotProbabilities},
+ samples =
+  Table[unitize[
+    Counts[RandomChoice[
+      Join @@ (Table[# - 1, #] & /@
+         RandomChoice[probabilities, 1000]), 100000]]], 100];
+ plotProbabilities =
+  associateLists[Values[probabilities], Keys[probabilities]];
+ ListLinePlot[samples~Join~{plotProbabilities},
+  GridLines -> {Range@50, Range[0, 0.20, 0.01]}, ImageSize -> 788,
+  InterpolationOrder -> 2,
+  PlotStyle ->
+   Table[{Thickness[0], Opacity[0.2], Green}, Length@samples]~
+    Join~{{Red, Thick, Dashed}}, Mesh -> All,
+  MeshStyle -> {Red, PointSize[0]}, PlotRange -> {{0, 50}, {0, 0.15}}]
+ ]
---
+++ statistics.m
@@ -0,0 +1,34 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: statistics *)
+(* :Context: statistics` *)
+(* :Author: Alcatraz *)
+(* :Date: 2018-08-16 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2018 Alcatraz *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["statistics`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+unitize::"Takes in a list(association) and returns a list(association) whose sum(sum of values) equals one ";
+associateLists::"Takes two lists and MapThreads the first list to the second one and creates an association of of it";
+
+Begin["`Private`"]
+
+unitize[list_List] := N[list / Total[list]]
+unitize[association_Association] := Module[{sortedAssociation = KeySort[association],keys,values},
+  keys = Keys[sortedAssociation];
+  values = unitize[Values[sortedAssociation]];
+  Association[MapThread[{#1->#2}&,{keys,values}]]
+]
+
+associateLists[list1_List,list2_List]:=Association[MapThread[{#1->#2}&,{list1,list2}]]
+
+End[]; (* `Private` *)
+
+EndPackage[];
