86e676f49d63475068ad01a5e16cf99edd5bbcfb
fermibot <ashwinuoh@gmail.com>
2018-08-28 06:47:44 -0500

    Created some files for Chapter 02 and moves files for chapter 03

---
+++ sheldon_ross/chapter_02/sheldon_ross_10_example_2.10.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: sheldon_ross_10_example_2 *)
+(* :Context: sheldon_ross_10_example_2` *)
+(* :Author: Alcatraz *)
+(* :Date: 2018-08-28 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2018 Alcatraz *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["sheldon_ross_10_example_2`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
