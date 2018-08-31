aa0e08b167426516832e58c397fab9bee5886159
fermibot <ashwinuoh@gmail.com>
2018-08-30 20:20:43 -0500

    Changes to Monte Carlo 5.01 (*Fully working version*)

--- i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_5.01.m
+++ i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_5.01.m
@@ -1,14 +1,29 @@
 ClearAll[status, currentStatus];
-status[list_List, time_] := Or @@ (Min[#] < time <= Max[#] & /@ list)
-currentStatus[list_List, time_] :=
-    Min[#] < time <= Max[#] &[Last[list]]
-status[#, 10] & /@ ConstantArray[0, {10, 2, 2}]
-currentStatus[#, 10] & /@ ConstantArray[0, {10, 2, 2}]
+status[list_List, time_] :=
+    Boole[! Or @@ (Min[#] < time <= Max[#] & /@ list)]
+currentStatus[list_List, time_] := ! Min[#] < time <= Max[#] &[
+  Last[list]]
 
-
-Module[{servers = 10, serverStatus, nCustomers = 1000,
-  arrivals, \[Lambda]a = 5, \[Lambda]s = 15},
+Module[{servers = 10, serverStatus, nCustomers = 100,
+  arrivals, \[Lambda]a = 5, \[Lambda]s = 0.1, firstFree},
   arrivals =
       Accumulate[
         RandomVariate[ExponentialDistribution[\[Lambda]a], nCustomers]];
+  serverStatus = ConstantArray[{{0, 0}}, servers];
+
+  Quiet@Table[
+    firstFree =
+        First[Position[currentStatus[#, arrivals[[r]]] & /@ serverStatus,
+          True]][[1]];
+    If[Echo[firstFree] =!= Null,
+      firstFree = First[firstFree][[1]];
+      serverStatus[[firstFree]] =
+          serverStatus[[firstFree]] ~
+              Join ~ {{arrivals[[r]],
+            arrivals[[r]] +
+                RandomVariate[ExponentialDistribution[\[Lambda]s]]}};,
+      Print["No server free"]]
+    , {r, 1, Length@arrivals}];
+
+  Print /@ serverStatus
 ]
