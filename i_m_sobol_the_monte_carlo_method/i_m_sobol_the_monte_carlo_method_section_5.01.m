f80978080ef04f7c216819630d7bbccf9cf0cac6
fermibot <ashwinuoh@gmail.com>
2018-08-31 19:05:43 -0500

    More changes to Changes to Monte Carlo 5.01

--- i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_5.01.m
+++ i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_5.01.m
@@ -5,7 +5,7 @@ currentStatus[list_List, time_] := ! Min[#] < time <= Max[#] &[
   Last[list]]
 
 Module[{servers = 10, serverStatus, nCustomers = 120,
-  arrivals, \[Lambda]a = 5, \[Lambda]s = 1, firstFree},
+  arrivals, \[Lambda]a = 5, \[Lambda]s = #, firstFree, plot},
   arrivals =
       Accumulate[
         RandomVariate[ExponentialDistribution[\[Lambda]a], nCustomers]];
@@ -13,7 +13,8 @@ Module[{servers = 10, serverStatus, nCustomers = 120,
 
   Quiet@Table[
     firstFree =
-        First[Position[currentStatus[#, arrivals[[r]]] & /@ serverStatus,
+        First[Position[
+          currentStatus[#, arrivals[[r]]] & /@ serverStatus,
           True]][[1]];
     If[firstFree =!= Null,
       firstFree = First[firstFree][[1]];
@@ -25,9 +26,26 @@ Module[{servers = 10, serverStatus, nCustomers = 120,
       Print["No server free"]]
     , {r, 1, Length@arrivals}];
 
-  Column[Plot[Evaluate[status[#, r]], {r, 1, 100}, Exclusions -> None,
-    ExclusionsStyle -> Red, ImageSize -> 1200, AspectRatio -> 0.1,
-    PlotRange -> {{0, Max@arrivals}, {0, 1.1}}] & /@ serverStatus]
+  plot =
+      Labeled[Framed@
+          Column[MapThread[
+            Plot[status[#1, r], {r, 1, 100}, Exclusions -> None,
+              ExclusionsStyle -> Red, ImageSize -> 788,
+              AspectRatio -> 0.1, Frame -> True,
+              PlotRange -> {{0, Max@arrivals}, {0, 1.1}},
+              PlotLabel -> "Server " <> #2, FrameTicks -> False,
+              FrameLabel -> {None,
+                Style["NotBusy-Busy", Bold]}] &, {serverStatus,
+              ToString /@ Range[Length@serverStatus]}]],
+        "\!\(\*SubscriptBox[\(\[Lambda]\), \(a\)]\) = " <>
+            ToString[\[Lambda]a] <>
+            " : \!\(\*SubscriptBox[\(\[Lambda]\), \(s\)]\) = " <>
+            ToString[\[Lambda]s], Top];
 
-
-]
+  Export[
+    StringReplace[NotebookFileName[],
+      ".nb" ->
+          "_rates_" <> ToString[\[Lambda]a] <> "_" <>
+              ToString[\[Lambda]s] <> ".png"], plot, ImageSize -> 788,
+    ImageResolution -> 1000]
+] & /@ Range[0.1, 5, 0.1]
