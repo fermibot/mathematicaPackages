c0c2a0611d2b91b54b6bb64f88af0c9ed19a6443
fermibot <ashwinuoh@gmail.com>
2018-09-04 19:50:37 -0500

    Updates to I. M. Sobol Section 07

--- i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_7.m
+++ i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_7.m
@@ -18,14 +18,47 @@ Module[{data =
   plotDataPassed = yRandomizer /@ passed;
   plotDataAbsorbed = yRandomizer /@ absorbed;
 
-  Column[Graphics[
+  Echo[Length /@ {passed, reflected, absorbed}];
+
+  Framed@Column[Graphics[
     {
-      {Red, Opacity[0.1], Thickness@0, #[plotDataReflected]},
-      {Blue, Opacity[0.1], Thickness@0, #[plotDataPassed]},
-      {Green, Opacity[0.2], Thickness@0, #[plotDataAbsorbed]},
+      {Red, Opacity[0.1], Arrowheads@0.02,
+        Thickness@0, #[plotDataReflected]},
+      {Gray, Opacity[0.04], Arrowheads@0.02,
+        Thickness@0, #[plotDataPassed]},
+      {Gray, Opacity[0.04], Arrowheads@0.02,
+        Thickness@0, #[plotDataAbsorbed]},
       {InfiniteLine[{{0, -1}, {0, 1}}]},
       {InfiniteLine[{{2, -1}, {2, 1}}]}
     }
-    , PlotRange -> All, ImageSize -> 788] & /@ {Line, BezierCurve,
-    BSplineCurve}]
+    , PlotRange -> All, ImageSize -> 788,
+    PlotLabel ->
+        "Reflected Neutrons: " <> ToString@# <>
+            " representation"] & /@ {Arrow, BSplineCurve}]
+]
+
+(*Need to run python code before running this one*)
+Module[{multiSimData,
+  printTemporary = PrintTemporary["Beginning analysis"]},
+  multiSimData =
+      Module[{data = DeleteCases[Import[#], {}], collisions, last,
+        reflected, absorbed, passed, plotDataReflected, plotDataPassed,
+        plotDataAbsorbed},
+        collisions = {Length[#], Last[#]} & /@ data;
+        last = Last /@ data;
+        reflected = Select[data, (Last[#] < 0) &];
+        absorbed = Select[data, (0 <= Last@# <= 2) &];
+        passed = Select[data, (Last@# > 2) &];
+        plotDataReflected = yRandomizer /@ reflected;
+        plotDataPassed = yRandomizer /@ passed;
+        plotDataAbsorbed = yRandomizer /@ absorbed;
+        NotebookDelete[printTemporary];
+        printTemporary =
+            PrintTemporary[Style["Analyzing the file " <> #, Darker@Green]];
+        Length /@ {passed, reflected, absorbed}
+      ] & /@
+          SortBy[FileNames["*temp*", NotebookDirectory[]],
+            ToExpression@Last[StringSplit[StringDelete[#, ".csv"], "_"]] &];
+  Export[StringReplace[NotebookFileName[], ".nb" -> ".xlsx"],
+    multiSimData]
 ]
