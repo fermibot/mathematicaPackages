d071a6f224040792e467650fe9457690ee8b044c
fermibot <ashwinuoh@gmail.com>
2018-09-10 20:09:00 -0500

    updates to section 3.6.2

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_section_3.6.2.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_section_3.6.2.m
@@ -2,14 +2,18 @@ AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
 << lists`;
 << utilities`;
 
-ListPlot[Module[{nNodes = Union @@ (Range[10] * 10^# & /@ Range[0, 2]),
-  graphTruth},
-  graphTruth =
-      lists`listProportion[#,
-        True] & /@ (Table[Module[{nodes = Range@#, graph},
+Module[{plotData, nNodes = Union @@ (Range[10] * 10^# & /@ Range[0, 1])},
+  plotData = Module[{graphTruth},
+    graphTruth =
+        lists`listProportion[#,
+          True] & /@ (Table[Module[{nodes = Range@#, graph},
 
-        ConnectedGraphQ[
-          Graph[# \[UndirectedEdge] RandomChoice[nodes] & /@ nodes]]
-      ], 20] & /@ nNodes);
-  associateLists[nNodes, graphTruth]
-] & /@ Range@10]
+          ConnectedGraphQ[
+            Graph[# \[UndirectedEdge] RandomChoice[nodes] & /@ nodes]]
+        ], 1000] & /@ nNodes);
+    associateLists[nNodes, graphTruth]
+  ] & /@ Range@20;
+  ListLogLogPlot[plotData, Joined -> True,
+    PlotStyle -> Table[{Green // Darker, Thickness@0}, Length@plotData],
+    InterpolationOrder -> 2, ImageSize -> 788,
+    Ticks -> {nNodes, Range[0, 1, 0.1]}]]
