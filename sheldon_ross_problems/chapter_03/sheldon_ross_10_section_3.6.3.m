059bb4975376e1fdfdb2bc83722d4cb39f084a72
fermibot <ashwinuoh@gmail.com>
2018-09-20 20:55:28 -0500

    updates to section 3.6.3 - re-wrote the math text

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_section_3.6.3.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_section_3.6.3.m
@@ -5,9 +5,9 @@ Needs["utilities`"];
 Uniform Priors
 
 Gamma[n + 1] / (Gamma[k + 1] Gamma[n - k + 1]) \!\(
-\ * SubsuperscriptBox[\(\[Integral]\), \(0\), \(1\)]\(
-\ * SuperscriptBox[\(p\), \(k\)]
-\ * SuperscriptBox[\((1 - p)\), \(n - k\)]\[DifferentialD]p\)\)
+\*SubsuperscriptBox[\(\[Integral]\), \(0\), \(1\)]\(
+\*SuperscriptBox[\(p\), \(k\)]
+\*SuperscriptBox[\((1 - p)\), \(n - k\)]\[DifferentialD]p\)\)
 
 ListPlot[{Gamma[# + 1] / Gamma[# + 2] & /@ Range[100],
   1 / (# + 1) & /@ Range[100]}, PlotRange -> All, ImageSize -> 700]
