f67e988bfe86a7a34c275b36011946b0ee8c7279
fermibot <ashwinuoh@gmail.com>
2018-10-23 20:45:15 -0500

    Updates to 3.022

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.022.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.022.m
@@ -2,14 +2,29 @@ AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
 Needs["utilities`"];
 
 Module[{data},
-  data = ({Last@#, Length@#} & /@ ToExpression /@ StringSplit[Import[#], "\n"]) &[#] & /@ FileNames["*.txt", NotebookDirectory[]];
+  data = ({Last@#, Length@#} & /@ ToExpression /@ StringSplit[Import[#], "\n"]) &[#] & /@ FileNames["*3.22_10_*.txt", NotebookDirectory[]];
   data = GroupBy[#, #[[1]] &] & /@ data;
   data = KeySort[(#[[All, 2]] & /@ # &[#])] &[#] & /@ data;
-  Column[
-    Framed[
-      DistributionChart[data,
-        ImageSize -> 800,
-        ChartElementFunction -> #,
-        ChartLabels -> {Style[#, 16, Red] & /@ {"k = 3\n", "k = 4\n"}, Keys@data[[1]]}]
-    ] & /@ {"SmoothDensity", "PointDensity", "GlassQuantile"}]
+  Column[Framed[
+    DistributionChart[data, ImageSize -> 800,
+      ChartElementFunction -> #,
+      ChartLabels -> {
+        Style[#, 16, Red] & /@ {"k = 3\n", "k = 4\n", "k = 5\n"},
+        Keys@data[[1]]}]] & /@ {"SmoothDensity", "PointDensity", "GlassQuantile"}
+  ]
+]
+
+Module[{data},
+  data = ({Last@#, Length@#} & /@ ToExpression /@ StringSplit[Import[#], "\n"]) &[#] & /@ FileNames["*3.22_15_*.txt", NotebookDirectory[]];
+  data = GroupBy[#, #[[1]] &] & /@ data;
+  data = KeySort[(#[[All, 2]] & /@ # &[#])] &[#] & /@ data;
+  Column[Framed[
+    DistributionChart[
+      data,
+      ImageSize -> 800,
+      ChartElementFunction -> #,
+      ChartLabels -> {
+        Style[#, 16, Red] & /@ {"k = 3\n", "k = 4\n", "k = 5\n"},
+        Keys@data[[1]]}]] & /@ {"SmoothDensity", "PointDensity", "GlassQuantile"}
+  ]
 ]
