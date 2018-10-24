1c5bbdf568fb97aa7dd7228934aebf4a18569275
fermibot <ashwinuoh@gmail.com>
2018-10-23 22:32:25 -0500

    Updates to 3.023

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.023.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.023.m
@@ -7,15 +7,16 @@ Module[{data},
       KeySort@
           Association@
               Table[
-                {p -> Module[{megaList = {}},
-                  Module[{list = RandomChoice[{p, 1 - p} -> {0, 1}, 2]},
-                    While[True,
-                      If[list[[{-2, -1}]] == {1, 1},
-                        AppendTo[megaList, list]; Break[],
-                        AppendTo[list, RandomChoice[{p, 1 - p} -> {0, 1}]]]]
-                  ]; & /@ Range[1000];
-                  Length /@ megaList
-                ]}
+                {p ->
+                    Module[{megaList = {}},
+                      Module[{list = RandomChoice[{p, 1 - p} -> {0, 1}, 2]},
+                        While[True,
+                          If[list[[{-2, -1}]] == {1, 1},
+                            AppendTo[megaList, list]; Break[],
+                            AppendTo[list, RandomChoice[{p, 1 - p} -> {0, 1}]]]]
+                      ]; & /@ Range[1000];
+                      Length /@ megaList
+                    ]}
                 , {p, 0.05, 0.80, 0.05}];
   DistributionChart[
     data,
