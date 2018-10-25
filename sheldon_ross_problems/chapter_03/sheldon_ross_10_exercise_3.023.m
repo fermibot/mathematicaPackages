8b67ced0667dc65d5ca8ff3fab424ca264cd0423
fermibot <ashwinuoh@gmail.com>
2018-10-24 21:19:02 -0500

    Updates to 3.023

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.023.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.023.m
@@ -2,26 +2,33 @@ AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
 SetOptions[$FrontEndSession,
   EvaluationCompletionAction -> {"ScrollToOutput"}]
 
+ClearAll[randomizer];
+
+randomizer[number_, percentage_ : 1] :=
+    number + RandomReal[0.01 percentage {-number, number}];
+
 Module[{data},
-  data =
-      KeySort@
-          Association@
-              Table[
-                {p ->
-                    Module[{megaList = {}},
-                      Module[{list = RandomChoice[{p, 1 - p} -> {0, 1}, 2]},
-                        While[True,
-                          If[list[[{-2, -1}]] == {1, 1},
-                            AppendTo[megaList, list]; Break[],
-                            AppendTo[list, RandomChoice[{p, 1 - p} -> {0, 1}]]]]
-                      ]; & /@ Range[1000];
-                      Length /@ megaList
-                    ]}
-                , {p, 0.05, 0.80, 0.05}];
+  data = KeySort@
+      Association@
+          Table[
+            {p ->
+                Module[{megaList = {}},
+                  Module[{list = {0} ~ Join ~ RandomChoice[{p, 1 - p} -> {0, 1}, 2]},
+                    While[True,
+                      If[Count[list[[-3 ;; -1]], 1] == 2,
+                        AppendTo[megaList, list]; Break[],
+                        AppendTo[list, RandomChoice[{p, 1 - p} -> {0, 1}]]]]
+                  ]; & /@ Range[1000];
+                  Length /@ megaList
+                ]}
+            , {p, 0.05, 0.80, 0.05}];
+
   DistributionChart[
-    data,
+    randomizer[#, 5] & /@ # & /@ data,
     ChartLabels -> Keys@data,
-    ChartElementFunction -> "SmoothDensity", ImageSize -> 788,
-    GridLines -> {None, Mean /@ (Values@data)}, AspectRatio -> 1
-  ]
+    ChartElementFunction -> "PointDensity",
+    ImageSize -> 788,
+    GridLines -> {None, Mean /@ (Values@data)},
+    AspectRatio -> 1,
+    PlotRange -> {All, All}]
 ]
