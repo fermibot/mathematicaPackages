f39c2343a5c7ee7b3709f4da036035ece2b8e469
fermibot <ashwinuoh@gmail.com>
2018-10-28 01:43:47 -0500

    Updates to 3.027

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.027.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.027.m
@@ -3,18 +3,20 @@ SetOptions[InstallJava, JVMArguments -> "-Xmx8g"];
 SetOptions[ReinstallJava, JVMArguments -> "-Xmx8g"];
 ReinstallJava[];
 AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
-SetOptions[$FrontEndSession,
-  EvaluationCompletionAction -> {"ScrollToOutput"}]
+SetOptions[$FrontEndSession, EvaluationCompletionAction -> {"ScrollToOutput"}]
 
 Module[
-  {data = Transpose@ Import[FileNames["*.csv", NotebookDirectory[]][[1]]],
-    chartLabels = ("\!\(\*SubscriptBox[\(p\), \(A\)]\) = " <> ToString[#]) & /@ Range[0.1, 0.9, 0.1], max = 600},
+  {data = Transpose@Import[FileNames["*.csv", NotebookDirectory[]][[1]]],
+    chartLabels = (Rotate["\!\(\*SubscriptBox[\(p\), \(A\)]\) = " <> ToString[#], Divide[\[Pi], 8]]) & /@ Range[0.1, 0.9, 0.05],
+    max = 500},
+
   DistributionChart[
     data,
     ImageSize -> 788,
     ChartElementFunction -> "SmoothDensity",
     ChartBaseStyle -> Opacity[0.5],
-    GridLines -> {None, Range[25, max, 25]}, ChartLabels -> chartLabels,
+    GridLines -> {None, Range[25, max, 25]},
+    ChartLabels -> chartLabels,
     AspectRatio -> 1,
     PlotRange -> {All, {0, max}}
   ]
