5b596728667af5ce691c44bc3eaa11893e9e0812
fermibot <ashwinuoh@gmail.com>
2018-10-27 22:13:02 -0500

    Updates to 3.026

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.026.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.026.m
@@ -10,11 +10,21 @@ Module[
   {data =
       Partition[
         Transpose@Import[FileNames["*.csv", NotebookDirectory[]][[1]]],
-        9]},
+        9], chartLabels = ("\!\(\*SubscriptBox[\(p\), \(A\)]\) = " <>
+      ToString[#]) & /@ Range[0.1, 0.9, 0.1]},
 
-  DistributionChart[data,
-    ImageSize -> 788,
-    ChartElementFunction -> "SmoothDensity",
-    ChartLayout -> "Overlapped", ChartBaseStyle -> Opacity[0.5],
-    GridLines -> {None, Range[80, 800, 80]}, AspectRatio -> 1]
+  Framed /@
+      Column /@
+          Partition[
+            MapThread[
+              DistributionChart[
+                #1, ImageSize -> 788,
+                ChartElementFunction -> "SmoothDensity",
+                ChartBaseStyle -> Opacity[0.5],
+                GridLines -> {None, Range[100, 600, 100]},
+                ChartLabels -> chartLabels, FrameLabel -> {{"", #2}, {"", ""}},
+                AspectRatio -> 0.25,
+                PlotRange -> {All, {0, 600}}] &,
+              {data, ("\!\(\*SubscriptBox[\(p\), \(B\)]\) = " <> ToString[#]) & /@ Range[0.1, 0.9, 0.1]}
+            ], 3]
 ]
