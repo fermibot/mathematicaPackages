840e3f254f4bea0b9d1618abe7cc57cd8f25ee27
fermibot <ashwinuoh@gmail.com>
2018-10-27 22:24:31 -0500

    Updates to 3.026

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.026.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.026.m
@@ -9,22 +9,25 @@ SetOptions[$FrontEndSession,
 Module[
   {data =
       Partition[
-        Transpose@Import[FileNames["*.csv", NotebookDirectory[]][[1]]],
-        9], chartLabels = ("\!\(\*SubscriptBox[\(p\), \(A\)]\) = " <>
-      ToString[#]) & /@ Range[0.1, 0.9, 0.1]},
+        Transpose@
+            Import[FileNames["*.csv", NotebookDirectory[]][[1]]],
+        9],
+    chartLabels = ("\!\(\*SubscriptBox[\(p\), \(A\)]\) = " <> ToString[#]) & /@ Range[0.1, 0.9, 0.1], max = 500},
 
-  Framed /@
-      Column /@
-          Partition[
-            MapThread[
-              DistributionChart[
-                #1, ImageSize -> 788,
-                ChartElementFunction -> "SmoothDensity",
-                ChartBaseStyle -> Opacity[0.5],
-                GridLines -> {None, Range[100, 600, 100]},
-                ChartLabels -> chartLabels, FrameLabel -> {{"", #2}, {"", ""}},
-                AspectRatio -> 0.25,
-                PlotRange -> {All, {0, 600}}] &,
-              {data, ("\!\(\*SubscriptBox[\(p\), \(B\)]\) = " <> ToString[#]) & /@ Range[0.1, 0.9, 0.1]}
-            ], 3]
+  Column[
+    Framed[#, FrameStyle -> GrayLevel[0.8]] & /@
+        Column /@
+            Partition[
+              MapThread[
+                DistributionChart[#1, ImageSize -> 788,
+                  ChartElementFunction -> "SmoothDensity",
+                  ChartBaseStyle -> Opacity[0.5],
+                  GridLines -> {None, Range[100, max, 100]},
+                  ChartLabels -> chartLabels,
+                  FrameLabel -> {{"", #2}, {"", ""}}, AspectRatio -> 0.25,
+                  PlotRange -> {All, {0, max}}
+                ] &, {data, ("\!\(\*SubscriptBox[\(p\), \(B\)]\) = " <> ToString[#]) & /@ Range[0.1, 0.9, 0.1]}
+              ]
+              , 3]
+  ]
 ]
