48b1f1b677b6d1cb169a494d3995e1bedae87796
fermibot <ashwinuoh@gmail.com>
2018-12-03 18:23:12 -0600

    Adding sheldon ross exercise 3.034 to the repository

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.034.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.034.m
@@ -1,14 +1,23 @@
-Module[{turns = {}},
-  Table[Module[
-    {dice := RandomChoice[{1, 2, 3, 4, 5, 6}], rolls, turn},
-    rolls = Table[dice, 10];
-    turn = 0;
-    While[Length@rolls > 0,
-      rolls = DeleteCases[rolls, 6];
-      rolls = Table[dice, Length@rolls];
-      turn += 1;
-    ];
-    AppendTo[turns, turn];
-  ], 100];
-  turns
+ClearAll[problem3034];
+problem3034[rangeIn_ : 6, numberIn_ : 10, iterations_] :=
+    Module[{turns = {}, range = Range[rangeIn], number = numberIn},
+      Table[Module[
+        {dice := RandomChoice[range], rolls, turn},
+        rolls = Table[dice, number];
+        turn = 0;
+        While[Length@rolls > 0,
+          rolls = DeleteCases[rolls, 6];
+          rolls = Table[dice, Length@rolls];
+          turn += 1;
+        ];
+        AppendTo[turns, turn];
+      ], iterations];
+      turns
+    ]
+
+Module[{sets = {1, 2, 5, 10, 20, 25, 40, 50, 100}},
+  DistributionChart[Table[problem3034[6, n, 10000], {n, sets}],
+    ChartElementFunction -> "SmoothDensity", ImageSize -> 788,
+    ChartLabels -> ("n = " <> ToString[#] & /@ sets),
+    PlotRange -> {Automatic, {0, 60}}]
 ]
