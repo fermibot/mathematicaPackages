6617245d4bd9e4b92b8d09862f41802033aaf4c9
fermibot <ashwinuoh@gmail.com>
2018-12-11 18:40:09 -0600

    Modifications to 3.049

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.049.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.049.m
@@ -1,18 +1,32 @@
-Module[{topSimulation, AWins, BWins},
-  topSimulation = Table[Module[{simulation},
-    simulation = Table[
-      Module[{a = 0, b = 0, c = 0, probability = 0.1},
-        While[True,
-          c += 1;
-          If[RandomReal[] <= probability, a += 1, b += 1];
-          If[Abs[a - b] >= 2, Break[]]];
-        {{"A", "B"}[[Position[{a, b}, Max[a, b]][[1, 1]]]], c}
-      ]
-      , 50];
-    Mean /@ (#[[2]] & /@ # & /@ GroupBy[simulation, #[[1]] &])
-  ], 1000];
-  AWins = #["A"] & /@ topSimulation;
-  BWins = #["B"] & /@ topSimulation;
-
-  DistributionChart[{AWins, BWins}]
-]
+Framed@
+    Grid@
+        Partition[
+          Table[
+            With[{p = prob},
+              Module[{topSimulation, AWins, BWins, chartPair},
+                chartPair = {AWins, BWins};
+                topSimulation = Table[
+                  Module[{simulation},
+                    simulation = Table[
+                      Module[{a = 0, b = 0, c = 0, probability = p},
+                        While[True,
+                          c += 1;
+                          If[RandomReal[] <= probability, a += 1, b += 1];
+                          If[Abs[a - b] >= 2, Break[]]];
+                        {{"A", "B"}[[Position[{a, b}, Max[a, b]][[1, 1]]]], c}
+                      ]
+                      , 100];
+                    Mean /@ (#[[2]] & /@ # & /@ GroupBy[simulation, #[[1]] &])
+                  ],
+                  1000];
+                {AWins, BWins} = {#["A"] & /@ topSimulation, #["B"] & /@ topSimulation};
+                DistributionChart[chartPair,
+                  ChartElementFunction -> "PointDensity",
+                  ChartLabels ->
+                      Placed[{{"A", "B"}, N@Mean@Select[#, NumberQ] & /@ {AWins, BWins}}, {Below, Above}],
+                  PlotRange -> {Automatic, {0, 10}},
+                  AspectRatio -> 1, ImageSize -> 260,
+                  PlotLabel -> Style["p = " <> ToString@prob, Italic]]
+              ]
+            ]
+            , {prob, Range[0.1, .9, 0.1]}], 3]
