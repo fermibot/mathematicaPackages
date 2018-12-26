c7a9877dc9da3a7d5c4e05f9764bb251cee69c0a
fermibot <ashwinuoh@gmail.com>
2018-12-25 21:54:18 -0600

    updates to 3.062

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.062.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.062.m
@@ -1,22 +1,37 @@
-Table[Module[{counter = 0,
-  gameTrack = <|"A" -> {}, "B" -> {}, "C" -> {}|>, waiting = "C",
-  current = {"A", "B"}, roundLoser, roundWinner, winner},
-  While[True,
-    counter += 1;
-    {roundLoser, roundWinner} = RandomSample[current, 2];
-    current = {roundWinner, waiting};
-    waiting = roundLoser;
-    MapThread[(gameTrack[#1] =
-        Append[gameTrack[#1], #2]) &, {{current[[1]], current[[2]],
-      waiting}, {1, 0, 0}}];
-    If[counter >= 2,
-      If[Or @@ (Take[#, -2] == {1, 1} & /@ gameTrack),
-        winner =
-            Piecewise[{#, Take[gameTrack[#], -2] == {1, 1}} & /@
-                Keys[gameTrack]];
-        AppendTo[outcomes, {counter, winner}];
-        Break[]]
-    ]
-  ]
-], runs];
-KeySort[Counts[#[[2]] & /@ outcomes] / runs // N]
+Module[{outcomes = {}, players = ToUpperCase[Alphabet[][[1 ;; 3]]],
+  printTemporary, megaRuns = 1000},
+  Table[
+    Module[{outcome = {}, runs = 1000},
+      printTemporary =
+          PrintTemporary[
+            "Simulating sequence: " <> ToString[r] <> " off " <>
+                ToString@megaRuns];
+      Table[Module[{counter = 0,
+        gameTrack = <|"A" -> {}, "B" -> {}, "C" -> {}|>, waiting = "C",
+        current = {"A", "B"}, roundLoser, roundWinner, winner},
+        While[True,
+          counter += 1;
+          {roundLoser, roundWinner} = RandomSample[current, 2];
+          current = {roundWinner, waiting};
+          waiting = roundLoser;
+          MapThread[(gameTrack[#1] =
+              Append[gameTrack[#1], #2]) &, {{current[[1]], current[[2]],
+            waiting}, {1, 0, 0}}];
+          If[counter >= 2,
+            If[Or @@ (Take[#, -2] == {1, 1} & /@ gameTrack),
+              winner =
+                  Piecewise[{#, Take[gameTrack[#], -2] == {1, 1}} & /@
+                      Keys[gameTrack]];
+              AppendTo[outcome, {counter, winner}];
+              Break[]]
+          ]
+        ]], runs];
+      AppendTo[outcomes, KeySort[Counts[#[[2]] & /@ outcome] / runs]];
+      NotebookDelete@printTemporary;
+    ], {r, 1, megaRuns}];
+
+  outcomes =
+      Association@
+          Table[player -> (#[player] & /@ outcomes), {player, players}];
+  DistributionChart[outcomes, "ChartElementFunction" -> "PointDensity"]
+]
