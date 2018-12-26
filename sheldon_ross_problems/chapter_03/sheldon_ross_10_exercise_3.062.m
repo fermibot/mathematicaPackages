6acdff66263106f5b4e4cf4231ef106b2dbc8751
fermibot <ashwinuoh@gmail.com>
2018-12-25 21:06:14 -0600

    updates to 3.062

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.062.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.062.m
@@ -1,15 +1,22 @@
-Module[{counter = 0, gameTrack = <|"A" -> {}, "B" -> {}, "C" -> {}|>,
-  waiting = "C", current = {"A", "B"}, loser, winner},
+Table[Module[{counter = 0,
+  gameTrack = <|"A" -> {}, "B" -> {}, "C" -> {}|>, waiting = "C",
+  current = {"A", "B"}, roundLoser, roundWinner, winner},
   While[True,
     counter += 1;
-    {loser, winner} = RandomSample[current, 2];
-    current = {winner, waiting};
-    waiting = loser;
+    {roundLoser, roundWinner} = RandomSample[current, 2];
+    current = {roundWinner, waiting};
+    waiting = roundLoser;
     MapThread[(gameTrack[#1] =
         Append[gameTrack[#1], #2]) &, {{current[[1]], current[[2]],
       waiting}, {1, 0, 0}}];
-    Print[gameTrack];
     If[counter >= 2,
-      If[Or @@ (Take[#, -2] == {1, 1} & /@ gameTrack), Break[]]];
+      If[Or @@ (Take[#, -2] == {1, 1} & /@ gameTrack),
+        winner =
+            Piecewise[{#, Take[gameTrack[#], -2] == {1, 1}} & /@
+                Keys[gameTrack]];
+        AppendTo[outcomes, {counter, winner}];
+        Break[]]
+    ]
   ]
-]
+], runs];
+KeySort[Counts[#[[2]] & /@ outcomes] / runs // N]
