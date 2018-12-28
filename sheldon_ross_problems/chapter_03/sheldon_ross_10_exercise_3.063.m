b548592a8e57c6328207a20c976cc1f4f418234c
fermibot <ashwinuoh@gmail.com>
2018-12-28 07:47:35 -0600

    updates to 3.063

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.063.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.063.m
@@ -28,27 +28,28 @@ Module[{plotData = List[], plotExtent = 15},
 
 
 Row[Riffle[#, "\t\t"]] &[
-  Framed[Grid[Partition[#, 3]], FrameStyle -> Directive[LightGray]] &[
-    Table[Module[{plotData = List[], plotExtent = 26},
-      Do[
-        Module[{coupons =
-            Sort@Take[Alphabet[] ~ Join ~ ToUpperCase[Alphabet[]], take],
-          newCoupon, couponCollection = List[], plotSubData},
-          newCoupon := RandomChoice@coupons;
-          While[True,
-            AppendTo[couponCollection, newCoupon];
-            If[Union@couponCollection == coupons, Break[]]
-          ];
-          plotSubData = KeySort@Counts@Values@Counts@couponCollection;
-          AppendTo[plotData, plotSubData]
-        ]
-        , 1000];
-      ListPlot[associationRandomizer /@ plotData, AspectRatio -> 1,
-        Frame -> True, PlotRange -> ConstantArray[{0, plotExtent}, 2],
-        PlotStyle -> {{PointSize@0.0, Darker@Green}},
-        GridLinesStyle -> Directive[Lighter@Gray, Dashed],
-        ImageSize -> 200,
-        Epilog -> {Text[
-          "Unique coupons \nfor taking = " <> ToString[take], {18,
-            21}]}]
-    ], {take, #}]] & /@ {Range[4, 26, 2], Range[28, 52, 2]}]
+  Parallelize[
+    Framed[Grid[Partition[#, 3]], FrameStyle -> Directive[LightGray]] &[
+      Table[Module[{plotData = List[], plotExtent = 20},
+        Do[
+          Module[{coupons =
+              Sort@Take[Alphabet[] ~ Join ~ ToUpperCase[Alphabet[]], take],
+            newCoupon, couponCollection = List[], plotSubData},
+            newCoupon := RandomChoice@coupons;
+            While[True,
+              AppendTo[couponCollection, newCoupon];
+              If[Union@couponCollection == coupons, Break[]]
+            ];
+            plotSubData = KeySort@Counts@Values@Counts@couponCollection;
+            AppendTo[plotData, plotSubData]
+          ]
+          , 1000];
+        ListPlot[associationRandomizer /@ plotData, AspectRatio -> 1,
+          Frame -> True, PlotRange -> ConstantArray[{0, plotExtent}, 2],
+          PlotStyle -> {{PointSize@0.0, Darker@Green}},
+          GridLinesStyle -> Directive[Lighter@Gray, Dashed],
+          ImageSize -> 200,
+          Epilog -> {Text[
+            "Unique coupons \nfor taking = " <> ToString[take], {14,
+              16}]}]
+      ], {take, #}]] & /@ {Range[4, 26, 2], Range[28, 52, 2]}]]
