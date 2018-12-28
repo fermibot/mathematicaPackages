ffb6fdd7bd8f4247afab89b24e81bac9063b2074
fermibot <ashwinuoh@gmail.com>
2018-12-28 17:35:03 -0600

    updates to 3.090

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.090.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.090.m
@@ -9,3 +9,16 @@ Table[ParallelTable[
     ];
     i
   ], 10] // Mean // N, 10]
+
+
+Table[ParallelTable[
+  Module[{accidents = RandomVariate[PoissonDistribution[5], 4],
+    i = 4},
+    While[True,
+      accidents =
+          Rest[accidents] ~ Join ~ {RandomVariate[PoissonDistribution[5]]};
+      i += 1;
+      If[accidents == {2, 1, 0, 2}, Break[]]
+    ];
+    i
+  ], 10] // Mean // N, 10]
