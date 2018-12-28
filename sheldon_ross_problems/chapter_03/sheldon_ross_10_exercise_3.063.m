34483434fb43b0bfb0d98282c1bceccb2fd57313
fermibot <ashwinuoh@gmail.com>
2018-12-27 20:31:42 -0600

    updates to 3.063

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.063.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.063.m
@@ -1,13 +1,13 @@
 ClearAll[associationRandomizer];
 associationRandomizer[association_?AssociationQ, offset_Real : 0.3] :=
     MapThread[{#1 + RandomReal[{-offset, offset}], #2 +
-        RandomReal[{-offset, offset}]} &, {Keys@association,
-      Values@association}]
+        RandomReal[{-offset, offset}]} &, {Values@association,
+      Keys@association}]
 
 Module[{plotData = List[], plotExtent = 15},
   Do[
-    Module[{coupons = Alphabet[], newCoupon, couponCollection = List[],
-      plotSubData},
+    Module[{coupons = Take[Alphabet[], 26], newCoupon,
+      couponCollection = List[], plotSubData},
       newCoupon := RandomChoice@coupons;
       While[True,
         AppendTo[couponCollection, newCoupon];
@@ -18,10 +18,34 @@ Module[{plotData = List[], plotExtent = 15},
     ]
     , 2000];
   ListPlot[associationRandomizer /@ plotData, AspectRatio -> 1,
-    Frame -> True, GridLines -> ConstantArray[Range@plotExtent, 2],
-    PlotRange -> ConstantArray[{0, plotExtent}, 2],
-    FrameLabel -> (Style[#, Gray, 16] & /@ {"Repeating coupons",
-      "Number of such occurrences"}),
+    Frame -> True, PlotRange -> ConstantArray[{0, plotExtent}, 2],
+    GridLines -> ConstantArray[Range@plotExtent, 2],
     PlotStyle -> {{PointSize@0.002, Darker@Green}},
-    GridLinesStyle -> Directive[Lighter@Gray, Dashed], ImageSize -> 788]
+    GridLinesStyle -> Directive[Lighter@Gray, Dashed], ImageSize -> 788,
+    FrameLabel -> (Style[#, Gray, 16] & /@ {"Repeating coupons",
+      "Number of such occurrences"})]
 ]
+
+
+Grid[Partition[#, 3]] &[
+  Table[Module[{plotData = List[], plotExtent = 15},
+    Do[
+      Module[{coupons = Take[Alphabet[], take], newCoupon,
+        couponCollection = List[], plotSubData},
+        newCoupon := RandomChoice@coupons;
+        While[True,
+          AppendTo[couponCollection, newCoupon];
+          If[Union@couponCollection == coupons, Break[]]
+        ];
+        plotSubData = KeySort@Counts@Values@Counts@couponCollection;
+        AppendTo[plotData, plotSubData]
+      ]
+      , 1000];
+    ListPlot[associationRandomizer /@ plotData, AspectRatio -> 1,
+      Frame -> True, PlotRange -> ConstantArray[{0, plotExtent}, 2],
+      PlotStyle -> {{PointSize@0.0, Darker@Green}},
+      GridLinesStyle -> Directive[Lighter@Gray, Dashed],
+      ImageSize -> 200,
+      Epilog -> {Text[
+        "Unique coupons \nfor taking = " <> ToString[take], {10, 12}]}]
+  ], {take, Range[4, 26, 2]}]]
