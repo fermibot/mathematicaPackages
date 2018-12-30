75bb7949733fe19bc77501795cfae8992e7afc71
fermibot <ashwinuoh@gmail.com>
2018-12-30 04:55:08 -0600

    updates to 3.091

--- sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.091.m
+++ sheldon_ross_problems/chapter_03/sheldon_ross_10_exercise_3.091.m
@@ -1,13 +1,18 @@
 AppendTo[$Path, "D:\\Mathematica Files 4K\\mathematicaPackages"];
 Needs["utilities`"];
 
-accumulatingMean@Table[Module[{sequence = RandomChoice[{0, 1}, 8], i},
-  i = Length@sequence;
-  While[True,
-    If[sequence == {1, 0, 1, 1, 0, 1, 0, 1}, Break[]];
-    sequence = Rest[sequence] ~ Join ~ {RandomChoice[{0, 1}]};
-    i += 1;
-  ];
-  i
-], 1000] //
-    DistributionChart[#, "ChartElementFunction" -> "PointDensity"] &
+With[{runs = 10000},
+  accumulatingMean@Table[Module[{sequence = RandomChoice[{0, 1}, 8], i},
+    i = Length@sequence;
+    While[True,
+      If[sequence == {1, 0, 1, 1, 0, 1, 0, 1}, Break[]];
+      sequence = Rest[sequence] ~ Join ~ {RandomChoice[{0, 1}]};
+      i += 1;
+    ];
+    i
+  ], runs] //
+      DistributionChart[#, "ChartElementFunction" -> "PointDensity",
+        ImageSize -> 788,
+        ChartLegends ->
+            Placed["Distribution of the number of trials needed to get the \
+sequence {1,0,1,1,0,1,0,1} " <> ToString[runs] <> " runs", Below]] &]
