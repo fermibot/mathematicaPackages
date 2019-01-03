2ab40a8e3beced6f1adc25051016bf3d9b986614
fermibot <ashwinuoh@gmail.com>
2019-01-02 20:18:22 -0600

    updates to 4.002

--- sheldon_ross_10_problems/chapter_04/sheldon_ross_10_example_4.002.m
+++ sheldon_ross_10_problems/chapter_04/sheldon_ross_10_example_4.002.m
@@ -5,10 +5,10 @@ Table[Module[{matrix = {{p, 1 - p}, {1 - p, p}}, data,
       KeySort@Counts[
         RandomFunction[\[ScriptCapitalP], {0, iterations}][[2, 1, 1]]];
   BarChart[data, Frame -> True, ImageSize -> 262, AspectRatio -> 1,
-    PlotRange -> {Automatic, {0, iterations}},
+    PlotRange -> {Automatic, {0, 7000}},
     ChartLabels ->
         Placed[#@data & /@ {Values, Keys}, {Above, Bottom}],
-    Epilog -> Text[Style["p = " <> ToString[p], 12], {1.5, 9750}]]
+    Epilog -> Text[Style["p = " <> ToString[p], 12], {1.5, 6750}]]
 ], {p, Range[0.1, 0.9, 0.1]}] //
     Labeled[Grid[Partition[#, 3]],
       Rotate[Style["Number of iterations per Markov Chain: 10000", 14,
