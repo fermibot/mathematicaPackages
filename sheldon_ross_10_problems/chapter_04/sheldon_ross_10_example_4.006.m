1faf8e72c81b77c1adb053e312a6b2c67c6fbc66
fermibot <ashwinuoh@gmail.com>
2019-01-04 20:58:26 -0600

    updates to 4.006

--- sheldon_ross_10_problems/chapter_04/sheldon_ross_10_example_4.006.m
+++ sheldon_ross_10_problems/chapter_04/sheldon_ross_10_example_4.006.m
@@ -22,7 +22,7 @@ markovSimulator[transitionMatrix_?MatrixQ, currentState_Integer] :=
 circlePointsRandomizer[n_Integer] :=
     MapThread[{(1 + #2) Sin[Divide[2 \[Pi] #1, n]], (1 + #2) Cos[
       Divide[2 \[Pi] #1, n]]} &, {Range[n],
-      RandomReal[{-0.1, 0.1}, n]}]
+      RandomReal[{-0.2, 0.2}, n]}]
 
 
 chainObject[list_List, totalNodes_Integer] :=
@@ -31,30 +31,34 @@ chainObject[list_List, totalNodes_Integer] :=
     {Opacity@0.1, BSplineCurve[circleSubset, SplineDegree -> 2]}
     ]
 
-Module[{dimensions = 16, transitionMatrix, labels, p = 0.8,
-  iterations = 100, simulations = {}},
-  transitionMatrix = ConstantArray[0, ConstantArray[dimensions, 2]];
-  Table[If[(i == 1 && j == 1) || (i == dimensions && j == dimensions),
-    transitionMatrix[[i, j]] = 1,
-    If[(i == j + 1) && (i != dimensions),
-      transitionMatrix[[i, j]] = 1 - p,
-      If[(i == j - 1) && (i != 1), transitionMatrix[[i, j]] = p]]], {i,
-    1, dimensions}, {j, 1, dimensions}];
-  labels =
-      "$" <> ToString[#] & /@
-          Range[0, Dimensions[transitionMatrix][[1]] - 1];
-  labels[[-1]] = labels[[-1]] <> " Win";
-  labels[[1]] = labels[[1]] <> " Broke";
+Grid[Partition[#, 3]] &[
+  Table[Module[{dimensions = 16, transitionMatrix, labels, p = prob,
+    iterations = 100, simulations = {}},
+    transitionMatrix = ConstantArray[0, ConstantArray[dimensions, 2]];
+    Table[If[(i == 1 && j == 1) || (i == dimensions &&
+        j == dimensions), transitionMatrix[[i, j]] = 1,
+      If[(i == j + 1) && (i != dimensions),
+        transitionMatrix[[i, j]] = 1 - p,
+        If[(i == j - 1) && (i != 1),
+          transitionMatrix[[i, j]] = p]]], {i, 1, dimensions}, {j, 1,
+      dimensions}];
+    labels =
+        "$" <> ToString[#] & /@
+            Range[0, Dimensions[transitionMatrix][[1]] - 1];
+    labels[[-1]] = labels[[-1]] <> " Win";
+    labels[[1]] = labels[[1]] <> " Broke";
 
-  Table[Module[{simulation, start = 7},
-    simulation = {start};
-    While[True,
-      start = markovSimulator[transitionMatrix, start];
-      AppendTo[simulation, start];
-      If[start == 1 || start == 16, Break[]];
-    ];
-    AppendTo[simulations, simulation];
-  ], iterations];
+    Table[Module[{simulation, start = 7},
+      simulation = {start};
+      While[True,
+        start = markovSimulator[transitionMatrix, start];
+        AppendTo[simulation, start];
+        If[start == 1 || start == 16, Break[]];
+      ];
+      AppendTo[simulations, simulation];
+    ], iterations];
 
-  Graphics[chainObject[#, 16] & /@ simulations]
-]
+    Graphics[{chainObject[#, 16] & /@ simulations,
+      Text["p = " <> ToString[p], {0, 0}]},
+      PlotRange -> ConstantArray[{-1.3, 1.3}, 2], ImageSize -> 262]
+  ], {prob, Range[0.1, 0.9, 0.1]}]]
