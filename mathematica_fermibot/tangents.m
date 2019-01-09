05897cea1f00191ddef1b9a26c25d081ffb7e20b
fermibot <ashwinuoh@gmail.com>
2019-01-09 06:56:46 -0600

    shorter tangent code

--- mathematica_fermibot/tangents.m
+++ mathematica_fermibot/tangents.m
@@ -1,9 +1,14 @@
-Module[
-  {number = 10000, tang, norm, opacity = Opacity[0.02], rx, ry},
-  rx[t_] := (3 + 1.5 Sin[3 t]) Sin[t];
-  ry[t_] := (3 + 1.5 Sin[3 t]) Cos[t];
+ClearAll[tangentPlot]
+tangentPlot[rxIn_, ryIn_] := Module[
+  {number = 10000, tang, norm, opacity = Opacity[0.04], rx, ry},
+  rx[t_] = rxIn;
+  ry[t_] = ryIn;
+  Echo[rx];
+  Echo[ry];
   tang = D[{rx[t], ry[t]}, {t, 1}];
   norm = D[{rx[t], ry[t]}, {t, 2}];
+  Echo[tang]; Echo[norm];
+  Echo@tang[2];
   a = ParametricPlot[{rx, ry}, {t, 0, 2 \[Pi]}, Frame -> False,
     Axes -> False, PlotStyle -> {Hue[0.2], Specularity -> None},
     PlotPoints -> 21, Mesh -> None];
@@ -20,3 +25,5 @@ Module[
   c = Show[a, b, PlotRange -> All, Background -> Black,
     ImageSize -> 788]
 ]
+
+tangentPlot[(3 + 1.5 Sin[3 t]) Sin[t], (3 + 1.5 Sin[3 t]) Cos[t]]
