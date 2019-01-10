2274848f1d1f1e99eefae743a888ea81e9c5dc3f
fermibot <ashwinuoh@gmail.com>
2019-01-09 19:28:50 -0600

    added options to tangentPlot

--- mathematica_fermibot/tangents.m
+++ mathematica_fermibot/tangents.m
@@ -1,29 +1,52 @@
 ClearAll[tangentPlot]
-tangentPlot[rxIn_, ryIn_] := Module[
-  {number = 10000, tang, norm, opacity = Opacity[0.04], rx, ry},
+
+Options[tangentPlot] = {"TangentColor" -> Hue@0.5,
+  "TangentOpacity" -> 0.04, "NumberOfLines" -> 10000,
+  ImageSize -> 788, Background -> Black, "ShowNormalsOnly" -> False,
+  "NormalColor" -> Hue[0.5], "NormalOpacity" -> 0.04};
+
+tangentPlot[rxIn_, ryIn_, OptionsPattern[]] := Module[
+  {number = OptionValue["NumberOfLines"], tang, norm, rx, ry,
+    tangentGraphics, normalGraphics, curvePlot},
   rx[t_] = rxIn;
   ry[t_] = ryIn;
-  Echo[rx];
-  Echo[ry];
   tang = D[{rx[t], ry[t]}, {t, 1}];
   norm = D[{rx[t], ry[t]}, {t, 2}];
-  Echo[tang]; Echo[norm];
-  Echo@tang[2];
-  a = ParametricPlot[{rx, ry}, {t, 0, 2 \[Pi]}, Frame -> False,
-    Axes -> False, PlotStyle -> {Hue[0.2], Specularity -> None},
-    PlotPoints -> 21, Mesh -> None];
-  b = Graphics[{Hue@0.5, opacity,
-    Line[Flatten[
-      Table[{{rx[t], ry[t]}, {rx[t], ry[t]} + tang}, {t,
-        Divide[2 \[Pi], number], 2 \[Pi], Divide[2 \[Pi], number]}],
-      0]]}];
-  c = Graphics[{LightBlue, opacity,
-    Line[Flatten[
-      Table[{{rx[t], ry[t]}, {rx[t], ry[t]} + norm}, {t,
-        Divide[2 \[Pi], number], 2 \[Pi], Divide[2 \[Pi], number]}],
-      0]]}];(*Normal*)
-  c = Show[a, b, PlotRange -> All, Background -> Black,
-    ImageSize -> 788]
+  curvePlot =
+      ParametricPlot[{rx, ry}, {t, 0, 2 \[Pi]}, Frame -> False,
+        Axes -> False, PlotStyle -> {Hue[0.2], Specularity -> None},
+        PlotPoints -> 21, Mesh -> None];
+  tangentGraphics =
+      Graphics[{OptionValue["TangentColor"],
+        Opacity[OptionValue["TangentOpacity"]],
+        Line[Flatten[
+          Table[{{rx[t], ry[t]}, {rx[t], ry[t]} + tang}, {t,
+            Divide[2 \[Pi], number], 2 \[Pi], Divide[2 \[Pi], number]}],
+          0]]}];
+  normalGraphics =
+      Graphics[{LightBlue, Opacity[OptionValue["NormalOpacity"]],
+        Line[Flatten[
+          Table[{{rx[t], ry[t]}, {rx[t], ry[t]} + norm}, {t,
+            Divide[2 \[Pi], number], 2 \[Pi], Divide[2 \[Pi], number]}],
+          0]]}];(*Normal*)
+
+  Show[a,
+    If[OptionValue["ShowNormalsOnly"], normalGraphics,
+      tangentGraphics], PlotRange -> All,
+    Background -> OptionValue[Background],
+    ImageSize -> OptionValue[ImageSize]]
 ]
 
-tangentPlot[(3 + 1.5 Sin[3 t]) Sin[t], (3 + 1.5 Sin[3 t]) Cos[t]]
+tangentPlot[(3 + 1.5 Sin[3 t]) Sin[t], (3 + 1.5 Sin[3 t]) Cos[t],
+  "NumberOfLines" -> 10000]
+
+tangentPlot[(3 + Sin[4 t]) Sin[t], (3 + Sin[4 t]) Cos[t],
+  "NumberOfLines" -> 5000]
+
+tangentPlot[(3 + Sin[5 t]) Sin[t], (3 + Sin[5 t]) Cos[t],
+  "NumberOfLines" -> 10000, "TangentColor" -> Red]
+
+tangentPlot[(3 + Sin[6 t]) Sin[t], (3 + Sin[6 t]) Cos[t],
+  "NumberOfLines" -> 10000, "TangentColor" -> Hue[0.55]]
+
+tangentPlot[Sin[t], Sin[t] Cos[t]]
