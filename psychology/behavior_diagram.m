e903d42a265f5d2f508ca7e9d3f6884d496d77cc
fermibot <ashwinuoh@gmail.com>
2019-01-11 22:45:49 -0600

    behavior_diagram

---
+++ psychology/behavior_diagram.m
@@ -0,0 +1,9 @@
+TreePlot[
+  {
+    "Reverence" -> "Resistance to Change",
+    "Lack of proper physique" -> "Treating actors as drivers\n rather than resource",
+    "Unable to be authoritative towards\n actors" -> "Reverence",
+    "People watching movies \nwith the same template" -> "Resistance to Change",
+    "Unchanging political landscape" -> "Resistance to Change"
+  },
+  VertexLabeling -> True, DirectedEdges -> True, ImageSize -> 600]
