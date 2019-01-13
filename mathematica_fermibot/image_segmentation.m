38007e4b9d4467d110457df9fd8d27faeeac21c9
fermibot <ashwinuoh@gmail.com>
2019-01-12 20:04:24 -0600

    added image_segmentation

---
+++ mathematica_fermibot/image_segmentation.m
@@ -0,0 +1,28 @@
+(* Mathematica Source File  *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+(* :Author: Alcatraz *)
+(* :Date: 2019-01-12 *)
+
+img = ImageData[ExampleData[{"TestImage", "Girl3"}]];
+imgdim = Dimensions[img]
+Graphics[Table[{RGBColor[img[[m, n]]],
+  Triangle[{t = {.2 n, .2 (imgdim[[2]] - m)},
+    t + RandomReal[{-.4, .4}, 2],
+    t + RandomReal[{-.4, .4}, 2]}]}, {n, imgdim[[1]], 1, -1}, {m, 1,
+  imgdim[[2]], 1}], ImageSize -> 1200]
+
+img = ImageData[ExampleData[{"TestImage", "Mandrill"}]];
+imgdim = Dimensions[img]
+Graphics[Table[{RGBColor[img[[m, n]]],
+  Triangle[{t = {.2 n, .2 (imgdim[[2]] - m)},
+    t + RandomReal[{-.4, .4}, 2],
+    t + RandomReal[{-.4, .4}, 2]}]}, {n, imgdim[[1]], 1, -1}, {m, 1,
+  imgdim[[2]], 1}], ImageSize -> 1200]
+
+ExampleData["TestImage"]
+
+img = ImageData[ExampleData[{"TestImage", "Lena"}]];
+imgdim = Dimensions[img]
+Graphics[Table[{RGBColor[img[[m, n]]],
+  Disk[.04 {n, imgdim[[2]] - m}, 0.1]}, {n, imgdim[[1]], 1, -4}, {m,
+  1, imgdim[[2]], 4}], ImageSize -> 1200]
