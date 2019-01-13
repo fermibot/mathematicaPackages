e9b22de8fab602aa6e61e9a7f09603fc9a70a9d4
fermibot <ashwinuoh@gmail.com>
2019-01-13 10:39:43 -0600

    updates to envelope_of_lines

--- mathematica_fermibot/envelope_of_lines.m
+++ mathematica_fermibot/envelope_of_lines.m
@@ -75,3 +75,26 @@ With[{r = 0.25, pieces = 2000},
       Range[Divide[\[Pi], pieces], 2 \[Pi], Divide[\[Pi], pieces]],
     ImageSize -> 788]]
 (*Export[NotebookDirectory[] <> "envelope_08.png", %, ImageSize -> 788]*)
+
+
+Module[{step = Divide[\[Pi], 400], images, partition = 10},
+  images = With[{offset = #, iterations = 200},
+    Export[ToString[N@#] <> ".png", Graphics[{
+      Table[{
+        Opacity@0.1,
+        Line[
+          {{Cos@n, Sin@n},
+            {2 + Sin[n + 2 \[Pi] offset], Cos[n + 2 \[Pi] offset]},
+            {2 + Cos[n + 2 \[Pi] 2 offset],
+              2 + Sin[n + 2 \[Pi] 2 offset]},
+            {Sin[n + 2 \[Pi] 3 offset], 2 + Cos[n + 2 \[Pi] 3 offset]},
+            {Cos[n + 2 \[Pi] 4 offset], Sin[n + 2 \[Pi] 4 offset]}
+          }
+        ]}, {n, Divide[2 \[Pi], iterations], 2 \[Pi],
+        Divide[2 \[Pi], iterations]}]}, ImageSize -> 788,
+      PlotRange -> {{-1, 3}, {-1, 3}}], ImageDimensions -> 788,
+      ImageResolution -> 500]] & /@ Range[0, Pi / 4, step];
+  If[Mod[Length@images, partition] != 0,
+    images = PadRight[images,
+      Times[Quotient[Length@images, partition] + 1, partition], ""]];
+  Grid[Partition[images, partition]]]
