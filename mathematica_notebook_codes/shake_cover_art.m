c5629751090987931da96dace67e01d0af370c63
fermibot <ashwinuoh@gmail.com>
2019-01-20 07:38:21 -0600

    added shake_cover_art

---
+++ mathematica_notebook_codes/shake_cover_art.m
@@ -0,0 +1,11 @@
+(* Mathematica Source File  *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+(* :Author: Alcatraz *)
+(* :Date: 2019-01-20 *)
+(* :OriginalCreationDate: 2016-12-29 *)
+
+Graphics[
+  {
+    {Hue[0.2], Rectangle[{0, 0}, {2, 1}]},
+    {Text[Style["Sake", Red], {0.5, 0.5}]}
+  }]
