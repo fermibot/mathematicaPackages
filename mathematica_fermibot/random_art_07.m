d854d56f5b2ca141656cd133c46c194f8ba7e169
fermibot <ashwinuoh@gmail.com>
2019-01-20 21:56:06 -0600

    updates to random_art_07

--- mathematica_fermibot/random_art_07.m
+++ mathematica_fermibot/random_art_07.m
@@ -1,6 +1,11 @@
 ClearAll[digitPad];
 digitPad[number_, pad_, padObject_ : 0] :=
     StringPadLeft[ToString[number], pad, padObject]
+exportFileName :=
+    NotebookDirectory[] <> "random_art_07_" <>
+        digitPad[Max[
+          ToExpression[StringTake[StringDelete[#, ".png"], -2]] & /@
+              FileNames["*png", NotebookDirectory[]]] + 1, 2, "0"] <> ".png"
 
 Module[{export},
   export =
@@ -10,21 +15,32 @@ Module[{export},
               Table[{Hue[Divide[t, 2 \[Pi]]], Opacity@0.005,
                 Disk[{Sin[t], Cos[t]}, 0.5]}, {t, Divide[2 \[Pi], iterations],
                 2 \[Pi], Divide[2 \[Pi], iterations]}], ImageSize -> 788]];
-  Export[NotebookDirectory[] <> "random_art_07_" <>
-      digitPad[
-        Max[ToExpression[StringTake[StringDelete[#, ".png"], -2]] & /@
-            FileNames["*png", NotebookDirectory[]]] + 1, 2, "0"] <> ".png",
-    export]
+  Export[exportFileName, export]
 ]
 
 Module[{export},
   export =
-      Module[{iterations = 1000},
+      Module[{iterations = 2000},
         iterations =
             Graphics[
-              Table[{Hue[Divide[t, 2 \[Pi]]], Opacity@0.005,
-                Disk[{Sin[5 t], Cos[3 t]}, 0.5]}, {t,
+              Table[Rotate[{EdgeForm[{Black, Opacity[0.2], Thickness[0.001]}],
+                FaceForm[{Hue[0.4], Opacity@0.02}],
+                Rectangle[{Sin[t], Cos[t]}]}, t], {t,
                 Divide[2 \[Pi], iterations], 2 \[Pi],
                 Divide[2 \[Pi], iterations]}], ImageSize -> 788]];
-  export
+  Export[exportFileName, export];
 ]
+
+Module[{export},
+  export =
+      Module[{iterations = 500},
+        iterations =
+            Graphics[
+              Table[Rotate[{EdgeForm[{Black, Opacity[0.2],
+                Thickness[0.001]}], FaceForm[{Hue[0.4], Opacity@0.02}],
+                Rectangle[{Sin[t], Cos[t]}]}, t], {t,
+                Divide[2 \[Pi], iterations], 2 \[Pi],
+                Divide[2 \[Pi], iterations]}], ImageSize -> 788]];
+  Export[exportFileName, export];
+]
+
