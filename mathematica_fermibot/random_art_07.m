8d442ac1f3758adee5a7b70e7c86e965abac86bd
fermibot <ashwinuoh@gmail.com>
2019-01-20 20:54:31 -0600

    added random_art_07

---
+++ mathematica_fermibot/random_art_07.m
@@ -0,0 +1,30 @@
+ClearAll[digitPad];
+digitPad[number_, pad_, padObject_ : 0] :=
+    StringPadLeft[ToString[number], pad, padObject]
+
+Module[{export},
+  export =
+      Module[{iterations = 1000},
+        iterations =
+            Graphics[
+              Table[{Hue[Divide[t, 2 \[Pi]]], Opacity@0.005,
+                Disk[{Sin[t], Cos[t]}, 0.5]}, {t, Divide[2 \[Pi], iterations],
+                2 \[Pi], Divide[2 \[Pi], iterations]}], ImageSize -> 788]];
+  Export[NotebookDirectory[] <> "random_art_07_" <>
+      digitPad[
+        Max[ToExpression[StringTake[StringDelete[#, ".png"], -2]] & /@
+            FileNames["*png", NotebookDirectory[]]] + 1, 2, "0"] <> ".png",
+    export]
+]
+
+Module[{export},
+  export =
+      Module[{iterations = 1000},
+        iterations =
+            Graphics[
+              Table[{Hue[Divide[t, 2 \[Pi]]], Opacity@0.005,
+                Disk[{Sin[5 t], Cos[3 t]}, 0.5]}, {t,
+                Divide[2 \[Pi], iterations], 2 \[Pi],
+                Divide[2 \[Pi], iterations]}], ImageSize -> 788]];
+  export
+]
