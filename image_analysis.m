9638f5bbb35ce843f20258ab054ebfe295af3243
fermibot <ashwinuoh@gmail.com>
2019-02-05 20:43:15 -0600

    updates to image processing

--- image_analysis.m
+++ image_analysis.m
@@ -1,4 +1,10 @@
-Module[{images = FileNames["*", NotebookDirectory[]]},
-  Image[ImageTrim[Import[images[[1]]], {{395, 750}, {3059, 2122}}],
-    ImageSize -> 1200]
+Module[{images = FileNames["*jpg", NotebookDirectory[]], image,
+  reImage},
+  Table[image =
+      ImageData[ImageTrim[Import[imageIn], {{395, 1114}, {3059, 2122}}]];
+  reImage =
+      If[And @@ (# >= 0.50 & /@ #), {1, 1, 1}, {0, 0, 0}] & /@ # & /@
+          image // Image;
+  ImageCollage[Image /@ {image, reImage},
+    ImageSize -> 1200], {imageIn, images}]
 ]
