7b6f56ef1d265446604ea788e3e550ef0de490cc
fermibot <ashwinuoh@gmail.com>
2019-04-06 17:12:46 -0500

    added fitbitDataAnalysis

---
+++ mathematica_notebook_codes/fitbitDataAnalysis.m
@@ -0,0 +1,43 @@
+(* Mathematica Source File  *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+(* :Author: Alcatraz *)
+(* :Date: 2019-04-06 *)
+
+
+
+Needs["DatabaseLink`"];
+
+ClearAll[stringToTime];
+stringToTime[dateString_?StringQ] :=
+    DateObject[{dateString, {"Month", "//", "Day", "//", "Year", " ",
+      "Hour24", ":", "Minute", ":", "Second"}},
+      TimeZone -> "America/Chicago"];
+
+Module[{database, data, dataH},
+  database =
+      OpenSQLConnection[
+        JDBC["SQLite", "D:\\Programming\\_databases\\fitbitData.db"]];
+  data = SQLExecute[database,
+    "
+   	SELECT *, 'Light' AS [ActivityType]
+   	FROM lightlyActiveMinutes
+   	UNION
+   	SELECT *, 'Moderate'
+   	FROM moderatelyActiveMinutes
+   	UNION
+   	SELECT *, 'Very'
+   	FROM veryActiveMinutes
+   	ORDER BY ActivityType
+   ",
+    "ShowColumnHeadings" -> True];
+  dataH = First@data;
+  data = Rest@data;
+  data = GroupBy[{stringToTime@#[[1]], #[[2]], #[[3]]} & /@
+      data, #[[3]] &];
+  data = #[[All, ;; 2]] & /@ data;
+  DateListPlot[data, ImageSize -> 1600, AspectRatio -> 0.4,
+    Frame -> True,
+    FrameTicks -> {data[[1, All, 1]][[;; ;; 10]], Range[0, 500, 25]},
+    DateTicksFormat -> { "Day", "/", "MonthNameShort", "/", "Year"},
+    GridLines -> {data[[1, All, 1]][[;; ;; 5]], Range[0, 500, 10]}]
+]
