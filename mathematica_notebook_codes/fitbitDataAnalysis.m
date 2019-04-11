5897e7137df0aacf764d23fbeeb7c9042d215484
fermibot <ashwinuoh@gmail.com>
2019-04-10 21:22:07 -0500

    added sql queries as external files

--- mathematica_notebook_codes/fitbitDataAnalysis.m
+++ mathematica_notebook_codes/fitbitDataAnalysis.m
@@ -3,10 +3,10 @@
 (* :Author: Alcatraz *)
 (* :Date: 2019-04-06 *)
 
-
 Needs["DatabaseLink`"];
 ClearAll[stringToTime];
 stringToTime[dateString_?StringQ] := DateObject[{dateString, {"Month", "//", "Day", "//", "Year", " ", "Hour24", ":", "Minute", ":", "Second"}}, TimeZone -> "America/Chicago"];
+
 
 Module[{folder =
     "C:\\Users\\Alcatraz\\Downloads\\MyFitbitData\\\
@@ -14,23 +14,12 @@ AshwiniKumarKounduri\\user-site-export\\"},
   First[StringSplit[StringDelete[#, folder], "-"]] & /@
       FileNames["*", folder] // Union // Print;]
 
-
 Module[{database, data, dataH},
   database =
       OpenSQLConnection[
         JDBC["SQLite", "D:\\Programming\\_databases\\fitbitData.db"]];
   data = SQLExecute[database,
-    "
-   	SELECT *, 'Light' AS [ActivityType]
-   	FROM lightlyActiveMinutes
-   	UNION
-   	SELECT *, 'Moderate'
-   	FROM moderatelyActiveMinutes
-   	UNION
-   	SELECT *, 'Very'
-   	FROM veryActiveMinutes
-   	ORDER BY ActivityType
-   ",
+    Import[NotebookDirectory[] <> "activeMinutes.txt"],
     "ShowColumnHeadings" -> True];
   dataH = First@data;
   data = Rest@data;
@@ -48,12 +37,7 @@ Module[{database, data, dataH},
 Module[{database, data, dataH},
   database = OpenSQLConnection["fitbit"];
   data = SQLExecute[database,
-    "SELECT CAST(dateTimeID AS VARCHAR) AS dateTimeID,
-          IN_DEFAULT_ZONE_1,
-          IN_DEFAULT_ZONE_2,
-          IN_DEFAULT_ZONE_3,
-          BELOW_DEFAULT_ZONE_1
-   FROM timeInHRZones"];
+    Import[NotebookDirectory[] <> "defaultZones.txt"]];
   data = {stringToTime[#[[1]]]} ~ Join ~ Rest[#] & /@ data;
   DateListPlot[
     Association@{"IN_DEFAULT_ZONE_1" ->
@@ -69,16 +53,10 @@ Module[{database, data, dataH},
     FrameLabel -> {"", "Time in Minutes"}]
 ]
 
-
 Module[{database, data, dataH, yMin = 0, yMax = 3, ySteps = 0.1},
   database = OpenSQLConnection["fitbit"];
-  data = SQLExecute[database, "
-   SELECT substr(datetimeID, 0, 9) || ' 00:00:00'
-        , avg(calories)
-        ,stdev(calories)
-   FROM calories
-   GROUP BY substr(datetimeID, 0, 9)
-   "];
+  data = SQLExecute[database,
+    Import[NotebookDirectory[] <> "calories.txt"]];
   data = {stringToTime[#[[1]]]} ~ Join ~ Rest[#] & /@ data;
   DateListPlot[
     Association[{"\[Mu] Calories" -> data[[;; , 1 ;; 2]],
@@ -94,18 +72,10 @@ Module[{database, data, dataH, yMin = 0, yMax = 3, ySteps = 0.1},
     FrameLabel -> {"", "Mean Calories"}, InterpolationOrder -> 2]
 ]
 
-
 Module[{database, data, dataH, yMin = 60, yMax = 105, ySteps = 2},
   database = OpenSQLConnection["fitbit"];
-  data = SQLExecute[database, "
-   SELECT substr(datetimeID, 0, 9) || ' 00:00:00'
-        , avg(bpm)
-        , avg(bpm) + avg(confidence)
-        , avg(bpm) - avg(confidence)
-   FROM heartRate
-   GROUP BY substr(datetimeID, 0, 9)
-
-   "];
+  data = SQLExecute[database,
+    Import[NotebookDirectory[] <> "heartRate.txt"]];
   data = {stringToTime[#[[1]]]} ~ Join ~ Rest[#] & /@ data;
   DateListPlot[
     Association[{"\[Mu] HeartRate" -> data[[;; , 1 ;; 2]],
@@ -119,6 +89,24 @@ Module[{database, data, dataH, yMin = 60, yMax = 105, ySteps = 2},
     PlotStyle -> {{Lighter@Blue, Thickness@0.001}, {Red, Opacity[0.4],
       Thickness@0}, {Green, Opacity[0.4], Thickness@0}},
     Filling -> {1 -> {2}, 1 -> {3}},
-    FillingStyle -> {{Opacity@0.1}, {Opacity@0.1}},
+    FillingStyle -> {{Opacity@0.1, Red}, {Opacity@0.1}},
     InterpolationOrder -> 2]
+]
+
+Module[{database, data, dataH, yMin = 0, yMax = 25000, ySteps = 1000},
+  database = OpenSQLConnection["fitbit"];
+  data = SQLExecute[database,
+    Import[NotebookDirectory[] <> "steps.txt"]];
+  data = SortBy[{stringToTime[#[[1]]]} ~ Join ~ Rest[#] & /@
+      data, #[[1]] &];
+
+  DateListPlot[Association[{"Total Steps" -> data[[;; , 1 ;; 2]]}],
+    ImageSize -> 1600, AspectRatio -> 0.4, Frame -> True,
+    FrameTicks -> {data[[All, 1]][[;; ;; 9]], Range[yMin, yMax, ySteps]},
+    GridLines -> {data[[All, 1]][[;; ;; 9]], Range[yMin, yMax, ySteps]},
+    PlotRange -> {Automatic, {yMin, yMax}},
+    DateTicksFormat -> { "Day", "/", "MonthNameShort", "/", "Year"},
+    PlotStyle -> {{Lighter@Blue, Thickness@0.001}, {Red, Opacity[0.4],
+      Thickness@0}, {Green, Opacity[0.4], Thickness@0}},
+    Filling -> Axis, FillingStyle -> LightBlue, InterpolationOrder -> 2]
 ]
