038fdf452308b9c84d1e03e11ba61472d1cbcdfb
fermibot <ashwinuoh@gmail.com>
2019-04-12 21:19:30 -0500

    added random_numbers_correlations from fermibot site

---
+++ mathematica_fermibot/random_numbers_correlations.m
@@ -0,0 +1,35 @@
+(* Mathematica Source File  *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+(* :Author: Alcatraz *)
+(* :Date: 2019-04-12 *)
+
+
+ClearAll[correlationPlot];
+correlationPlot[n_?IntegerQ] :=
+    Module[{tickGrid = {Range[-1.5, 1.5, 0.1], Range[0.1, 1.5, 0.1]},
+      sampleSize = n},
+      SmoothHistogram[
+        Table[Correlation[RandomReal[{0, 1}, sampleSize],
+          RandomReal[{0, 1}, sampleSize]], {100}, {1000}],
+        ImageSize -> 788, Frame -> True,
+        PlotRange -> {{-1.5, 1.5}, {0, 1.5}},
+        PlotStyle -> {{Red, Thickness -> 0, Opacity@0.1}},
+        GridLines -> tickGrid, FrameTicks -> tickGrid,
+        AspectRatio -> Divide[1, 2], Axes -> False,
+        PlotLabel -> "Sample Size = " <> ToString[sampleSize]]] //
+        Framed[#, FrameMargins -> 10, FrameStyle -> {GrayLevel[0.5]}] &
+
+correlationPlot[n_?ListQ] :=
+    Module[{tickGrid = {Range[-1.5, 1.5, 0.1], Range[0.1, 3, 0.1]},
+      sampleSize = n},
+      SmoothHistogram[(Table[
+        Correlation[RandomReal[{0, 1}, #], RandomReal[{0, 1}, #]],
+        10000] & /@ n), ImageSize -> 788, Frame -> True,
+        PlotRange -> {{-1.5, 1.5}, {0, 3}}, GridLines -> tickGrid,
+        FrameTicks -> tickGrid, AspectRatio -> Divide[1, 1],
+        Axes -> False, PlotLegends -> Placed[n, Top]]] //
+        Framed[#, FrameMargins -> 10, FrameStyle -> {GrayLevel[0.5]}] &
+
+correlationPlot[#] & /@ Range[3, 10]
+
+correlationPlot[{3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 20, 25, 30, 35, 40, 45, 50}]
