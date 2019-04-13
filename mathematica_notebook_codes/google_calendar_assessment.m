90bdff0f3582f06ff9d58ce61599636d72e17c5a
fermibot <ashwinuoh@gmail.com>
2019-04-13 17:39:40 -0500

    manually reformatted the code for readability

--- mathematica_notebook_codes/google_calendar_assessment.m
+++ mathematica_notebook_codes/google_calendar_assessment.m
@@ -6,18 +6,19 @@
 googleCalendar = ServiceConnect["GoogleCalendar"];
 
 MapThread[
-  With[{\[Delta] = #2}
-    , Module[{data, tickGrid},
-    tickGrid = {DateRange["2010", "2020", Quantity[2, #1]],
-      Automatic};
-    data = {#["Start"], #["End"], #["Summary"]} & /@
-        googlecalendar["EventList"];
-    TimelinePlot[(Interval[{#[[1]], #[[2]]}] -> #[[3]] & /@
-        Select[data, #[[1]] > DatePlus[Today, -\[Delta]] &]) ~
-        Join ~ {Now -> Style["Now", 15, Red]}, ImageSize -> 1600,
-      DateTicksFormat -> {"Year", "-", "MonthNameShort", "-", "Day",
-        " ", "Hour", ":", "Minute"}, Frame -> True,
-      GridLines -> tickGrid, Ticks -> tickGrid,
-      PlotRange -> {{DatePlus[Today, -#2], DatePlus[Today, #2]},
-        Automatic}]
-  ]] &, {{"Days", "Weeks"}, {5, 100}}] // Column
+  With[{\[Delta] = #2},
+    Module[{data, tickGrid},
+      tickGrid = {DateRange["2010", "2020", Quantity[2, #1]], Automatic};
+      data = {#["Start"], #["End"], #["Summary"]} & /@ googleCalendar["EventList"];
+
+      TimelinePlot[
+        (Interval[{#[[1]], #[[2]]}] -> #[[3]] & /@ Select[data, #[[1]] > DatePlus[Today, -\[Delta]] &])
+            ~ Join ~ {Now -> Style["Now", 15, Red]},
+        ImageSize -> 1600,
+        DateTicksFormat -> {"Year", "-", "MonthNameShort", "-", "Day", " ", "Hour", ":", "Minute"},
+        Frame -> True,
+        GridLines -> tickGrid,
+        Ticks -> tickGrid,
+        PlotRange -> {{DatePlus[Today, -#2], DatePlus[Today, #2]}, Automatic}]
+    ]
+  ] &, {{"Days", "Weeks"}, {5, 100}}] // Column
