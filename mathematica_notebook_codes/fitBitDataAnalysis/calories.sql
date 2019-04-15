d5548aec0221afb6db5261185c9ea28078538b0c
fermibot <ashwinuoh@gmail.com>
2019-04-14 19:07:47 -0500

    removed stdev from the calories query

--- mathematica_notebook_codes/fitBitDataAnalysis/calories.sql
+++ mathematica_notebook_codes/fitBitDataAnalysis/calories.sql
@@ -1,5 +1,5 @@
-SELECT substr(datetimeID, 0, 9) || ' 00:00:00'
-     , avg(calories)
-     ,stdev(calories)
+SELECT
+	substr(datetimeID, 0, 9) || ' 00:00:00',
+	avg(calories)
 FROM calories
 GROUP BY substr(datetimeID, 0, 9)
