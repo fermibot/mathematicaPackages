c323724ba898728aa130354fa30a5df4ff88496d
fermibot <ashwinuoh@gmail.com>
2019-04-16 21:08:16 -0500

    added washington trip to the google calendar assessment

--- mathematica_notebook_codes/google_calendar_assessment.m
+++ mathematica_notebook_codes/google_calendar_assessment.m
@@ -6,19 +6,38 @@
 googleCalendar = ServiceConnect["GoogleCalendar"];
 
 MapThread[
-  With[{\[Delta] = #2},
-    Module[{data, tickGrid},
-      tickGrid = {DateRange["2010", "2020", Quantity[2, #1]], Automatic};
-      data = {#["Start"], #["End"], #["Summary"]} & /@ googleCalendar["EventList"];
+  With[{\[Delta] = #2}
+    , Module[{data, tickGrid},
+    tickGrid = {DateRange["2010", "2020", Quantity[2, #1]],
+      Automatic};
+    data = {#["Start"], #["End"], #["Summary"]} & /@
+        googlecalendar["EventList"];
+    TimelinePlot[(Interval[{#[[1]], #[[2]]}] -> #[[3]] & /@
+        Select[data, #[[1]] > DatePlus[Today, -\[Delta]] && !
+            StringContainsQ[#[[3]], "IELTS"] &]) ~
+        Join ~ {Now -> Style["Now", 15, Red]}, ImageSize -> 1600,
+      DateTicksFormat -> {"Year", "-", "MonthNameShort", "-", "Day",
+        " ", "Hour", ":", "Minute"}, Frame -> True,
+      GridLines -> tickGrid, Ticks -> tickGrid,
+      PlotRange -> {{DatePlus[Today, -#2 / 4], DatePlus[Today, #2]},
+        Automatic}]
+  ]] &, {{"Days", "Weeks"}, {7, 100}}] // Column
 
-      TimelinePlot[
-        (Interval[{#[[1]], #[[2]]}] -> #[[3]] & /@ Select[data, #[[1]] > DatePlus[Today, -\[Delta]] &])
-            ~ Join ~ {Now -> Style["Now", 15, Red]},
-        ImageSize -> 1600,
-        DateTicksFormat -> {"Year", "-", "MonthNameShort", "-", "Day", " ", "Hour", ":", "Minute"},
-        Frame -> True,
-        GridLines -> tickGrid,
-        Ticks -> tickGrid,
-        PlotRange -> {{DatePlus[Today, -#2], DatePlus[Today, #2]}, Automatic}]
-    ]
-  ] &, {{"Days", "Weeks"}, {5, 100}}] // Column
+
+Module[{database, data, dataH, yMin = 0, yMax = 4200, ySteps = 100},
+  database = OpenSQLConnection["fitbit"];
+  data = SQLExecute[database,
+    Import["D:\\Programming\\mathematicaPackages\\mathematica_notebook_\
+codes\\fitBitDataAnalysis\\calories.sql"]];
+  data = {stringToTime[#[[1]]]} ~ Join ~ Rest[#] & /@ data;
+  DateListPlot[Association[{"Calories" -> data[[;; , 1 ;; 2]]}],
+    ImageSize -> 1600, AspectRatio -> 0.4, Frame -> True,
+    FrameTicks -> {Sort[data[[All, 1]]][[;; ;; 9]],
+      Range[yMin, yMax, ySteps]},
+    GridLines -> {Sort[data[[All, 1]][[;; ;; 3]]],
+      Range[yMin, yMax, ySteps]},
+    PlotRange -> {Automatic, {yMin, yMax}},
+    DateTicksFormat -> { "Day", "/", "MonthNameShort", "/", "Year"},
+    PlotStyle -> {{Automatic}, {Automatic, Opacity[0.4]}},
+    FrameLabel -> {"", "Calories"}, InterpolationOrder -> 1]
+]
