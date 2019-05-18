fbebbb2f09af0b879a080b93c3f2c36bf3d0e22d
fermibot <ashwinuoh@gmail.com>
2019-05-18 05:18:34 -0500

    added per minute and per hour steps

---
+++ mathematica_notebook_codes/fitBitDataAnalysis/minutely_HR_calories_steps.sql
@@ -0,0 +1,19 @@
+WITH HeartRateByHour AS (SELECT strftime('%Y-%m-%d %H:%M:00', dateTimeID) AS Hour, avg(bpm) AvgHeartRate
+                         FROM heartRate
+                         GROUP BY Hour),
+
+     CaloriesByHour AS (SELECT strftime('%Y-%m-%d %H:%M:00', dateTimeID) AS Hour, sum(calories) TotalCalories
+                        FROM calories
+                        GROUP BY Hour),
+
+     StepsByHour AS (SELECT strftime('%Y-%m-%d %H:%M:00', dateTimeID) AS Hour, sum(steps) AS TotalSteps
+                     FROM steps
+                     GROUP BY hour)
+SELECT AvgHeartRate
+     , TotalCalories
+     , TotalSteps
+FROM HeartRateByHour
+       LEFT OUTER JOIN CaloriesByHour
+                       ON CaloriesByHour.Hour = HeartRateByHour.Hour
+       LEFT OUTER JOIN StepsByHour ON StepsByHour.hour = HeartRateByHour.Hour
+ORDER BY StepsByHour.Hour DESC
